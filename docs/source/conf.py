4d95471a69b440be8aa884d15e02639f87790730
Kun-Lin Wu <49535725+calvin1126@users.noreply.github.com>
2021-10-14 16:28:12 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CO2-H2O-Zeolites-Adsorption'
+copyright = '2021, Kun-Lin'
+author = 'Kun-Lin'
 
 release = '0.1'
 version = '0.1.0'
